14295e2328a464f5867d544a2236f56057fd2d16
RCThakur <rinkeshthakur1997@gmail.com>
2025-06-15 12:17:45 +0530

    Setup React Router with protected and public routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 // src/App.jsx
 import { Routes, Route, Navigate } from "react-router-dom";
 import Inventory from "./pages/Inventory";
+import Landing from "./pages/Landing";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Dashboard from "./pages/Dashboard";
+import Reports from "./pages/Reports";
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Navigate to="/inventory" />} />
+      <Route path="/" element={<Landing />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/Dashboard" element={<Dashboard />} />
+      <Route path="/AddItem" element={<Navigate to="/Inventory" />} />
+      <Route path="/Reports" element={<Reports />} />
       <Route path="/inventory" element={<Inventory />} />
-      {/* You can add more pages later here like Reports, Dashboard, etc */}
     </Routes>
   );
 };
